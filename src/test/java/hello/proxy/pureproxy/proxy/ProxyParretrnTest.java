5e433be6a84fdce9b04d8785fff3e60559853418
carus-lee <leec8.2019@gmail.com>
2024-10-10 09:07:40 +0900

    프록시 패턴 - 예제코드2 > 프록시 통해서 접근제어 (속도개선)

--- src/test/java/hello/proxy/pureproxy/proxy/ProxyParretrnTest.java
+++ src/test/java/hello/proxy/pureproxy/proxy/ProxyParretrnTest.java
@@ -1,7 +1,9 @@
 package hello.proxy.pureproxy.proxy;
 
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
 import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
 import hello.proxy.pureproxy.proxy.code.RealSubject;
+import hello.proxy.pureproxy.proxy.code.Subject;
 import org.junit.jupiter.api.Test;
 
 public class ProxyParretrnTest {
@@ -10,6 +12,18 @@ public class ProxyParretrnTest {
     void noProxyTest() {
         RealSubject realSubject = new RealSubject();
         ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        //매 1초마다 응답
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void cacheProxyTest() {
+        Subject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+        //최초에만 1초가 소요되고 cacheProxy가 캐싱을 해서 응답속도(ms)가 빨라짐
         client.execute();
         client.execute();
         client.execute();
